26dc462ac4595d5e02e427cc462312e42bdf0921
ahnsihyun1202 <ahnsihyun1202@gmail.com>
2025-11-17 16:42:54 +0900

    주석 추가
    주석이 필요해 보이는 데는 일단 전부 추가했습니다
    필요하다면 조금 더 추가하거나 삭제하도록 하겠습니다

--- src/manager/QuizManager.java
+++ src/manager/QuizManager.java
@@ -17,7 +17,7 @@ public class QuizManager {
     static Random ran = new Random();
 
     ArrayList<String> noteWords = new ArrayList<>(); // 오답노트에 추가될 단어들
-    final User user;
+    final User user; //현재 사용자
     private String currentSourceFile = null;
 
     public QuizManager(User user) {
@@ -77,19 +77,22 @@ public class QuizManager {
     }
 
     private void createNote() { // 주어진 문제를 전부 풀고 난 뒤 오답노트 파일 만들기
-        if (noteWords.isEmpty()) {
+        if (noteWords.isEmpty()) { // 노트에 추가될 단어가 없으면
             System.out.println("틀린 단어가 없습니다.");
             return;
         }
 
+        //오답노트 파일 위치 가져오기
         File notes = new File(Path.getNoteDirPath(user.getName()));
 
+        //파일 이름을 현재 시간으로 설정
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HH_mm_ss");
         String formatted = now.format(formatter);
 
         File noteFile = new File(notes, "note-" + formatted + ".txt");
 
+        //텍스트 파일 내에 오답노트 단어들 전부 추가
         try (PrintWriter pw = new PrintWriter(
                 new OutputStreamWriter(new FileOutputStream(noteFile, false), StandardCharsets.UTF_8))) {
             for (String word : noteWords) {
@@ -100,13 +103,13 @@ public class QuizManager {
             System.out.println("파일 저장 중 오류 발생: " + e.getMessage());
         }
 
-        noteWords.clear();
+        noteWords.clear(); //다음 퀴즈 오답노트를 만들기 위해 오답노트에 추가할 단어 리스트는 비우기
     }
 
     private void addToNote(String aEng, String aKor) { // 주관/객관식 문제에서 오답이 나올때마다 오답노트에 추가
-        String entry = aEng + "\t" + aKor;
+        String entry = aEng + "\t" + aKor; // 각 단어는 영어 \t 한국어 형식으로 리스트에 추가
         if (!noteWords.contains(entry)) {
-            noteWords.add(entry);
+            noteWords.add(entry); //리스트에 중복되는 단어가 없을 때 리스트에 단어 추가
         }
     }
 
@@ -202,90 +205,96 @@ public class QuizManager {
     }
 
     private void shortAnswerQuestion(ArrayList<String> list) {
-        if (list == null) {
+        if (list == null) { //받은 리스트에 단어가 하나도 없으면
             System.out.println("단어가 등록되어 있지 않습니다.");
             return;
         }
 
+        //문제 수 입력
         System.out.print("문제 수를 입력해주세요 : ");
         int quizNum = sc.nextInt();
         sc.nextLine();
 
-        if (quizNum < 1) {
+        if (quizNum < 1) { //문제 수 입력할 때 0이나 음수를 적으면
             System.out.println("1문제 이상 출제되어야 합니다.");
             return;
-        } else if (quizNum > list.size()) {
-            quizNum = list.size();
+        } else if (quizNum > list.size()) { //입력한 문제 수가 가능한 문제 수보다 많으면
+            quizNum = list.size(); //문제 수를 가능한 최대의 문제 수로 설정
         }
 
-        boolean[] usedIndex = new boolean[list.size()];
-        int score = 0;
-        for (int i = 0; i < quizNum; i++) {
+        boolean[] usedIndex = new boolean[list.size()]; //중복되는 문제를 방지하기 위함
+        int score = 0; //사용자의 점수
+        for (int i = 0; i < quizNum; i++) { //문제 수만큼 반복
             int answerIndex;
             do {
-                answerIndex = ran.nextInt(list.size());
-            } while (usedIndex[answerIndex]);
-            usedIndex[answerIndex] = true;
+                answerIndex = ran.nextInt(list.size()); //리스트에 있는 랜덤의 단어 고르기
+            } while (usedIndex[answerIndex]); //중복되지 않을 때까지
+            usedIndex[answerIndex] = true; //성공적으로 고른 단어를 중복 표시
 
+            // aEng (영어), aKor (한국어) 나누기
             String[] a = list.get(answerIndex).split("\t", 2);
             String aEng = a[0].trim();
             String aKor = a[1].trim();
 
-            ArrayList<String> aKorList = new ArrayList<>();
+            ArrayList<String> aKorList = new ArrayList<>(); //한국어 뜻이 2개 이상인 경우를 위함
 
             if (aKor.contains("/")) {
                 String[] aKorArr = aKor.split("/");
                 for (String kor : aKorArr) {
-                    aKorList.add(kor.trim());
+                    aKorList.add(kor.trim()); // 슬래시를 기준으로 각 단어들을 리스트에 추가
                 }
             } else {
-                aKorList.add(aKor);
+                aKorList.add(aKor); // 슬래시가 없다면 한국어 뜻이 하나만 있으므로 aKor만 추가
             }
 
-            int randquiz = ran.nextInt(2) + 1;
-            if (randquiz == 1) {
+            int randquiz = ran.nextInt(2) + 1; //한국어 -> 영어 | 영어 -> 한국어 문제 형식 정하기
+            if (randquiz == 1) { //영어 -> 한국어
+                //문제 출력 (형식: [문제 번호/총 문제수] '영어'의 뜻은?)
                 System.out.println("\n[" + (i + 1) + "/" + quizNum + "] " + aEng + "의 뜻은?");
                 String answer = sc.nextLine().trim();
 
-                if (aKorList.contains(answer) || aKor.equals(answer)) {
+                if (aKorList.contains(answer) || aKor.equals(answer)) { //한국어 뜻 중 하나만 적거나 전부 적으면
                     System.out.println("정답!");
-                    score++;
+                    score++; //점수 증가
                 } else {
                     System.out.println("오답!");
-                    addToNote(aEng, aKor);
+                    addToNote(aEng, aKor); //오답노트에 추가될 단어를 리스트에 추가
 
-                    String answerStr = "";
+                    String answerStr = ""; //출력할 정답
                     for (String kor : aKorList) {
-                        answerStr += kor + "/ ";
+                        answerStr += kor + "/ "; //한국어 / 한국어2 / ... / 형식
                     }
-                    answerStr = answerStr.substring(0, answerStr.length() - 2);
-                    System.out.println("정답은 " + aEng + " = " + answerStr);
+                    answerStr = answerStr.substring(0, answerStr.length() - 2); //맨 마지막의 /를 지우기
+                    System.out.println("정답은 " + aEng + " = " + answerStr); //(형식: 정답은 '영어' = '한국어 / 한국어2 / ...)
                 }
 
-            } else {
+            } else { //한국어 -> 영어
 
+                // answerStr과 같은 원리
                 String questionStr = "";
                 for (String kor : aKorList) {
                     questionStr += kor + "/ ";
                 }
                 questionStr = questionStr.substring(0, questionStr.length() - 2);
 
+                //문제 출력 (형식: [문제 번호/총 문제수] '한국어'의 뜻은?)
                 System.out.println("\n[" + (i + 1) + "/" + quizNum + "] " + questionStr + "의 뜻은?");
                 String answer = sc.nextLine().trim();
 
-                if (answer.equals(aEng)) {
+                if (answer.equals(aEng)) { //입력한 영어와 같다면
                     System.out.println("정답!");
-                    score++;
+                    score++; //점수 증가
                 } else {
                     System.out.println("오답!");
-                    addToNote(aEng, aKor);
-                    System.out.println("정답은 " + aEng + " = " + questionStr);
+                    addToNote(aEng, aKor); //오답노트에 추가될 단어를 리스트에 추가
+                    System.out.println("정답은 " + aEng + " = " + questionStr); //(형식: 정답은 '영어' = '한국어 / 한국어2 / ...)
                 }
             }
 
         }
+        // (형식: 총 {문제 수}문제 중 {맞힌 개수}개 정답 (정답률 {소수점 첫째자리까지의 정답률})
         System.out.printf("\n총 %d문제 중 %d개 정답 (정답률 %.1f%%)\n", quizNum, score, 100.0 * score / quizNum);
-        createNote();
+        createNote(); //퀴즈가 끝난 뒤 오답노트 생성
     }
 
     private void multipleChoiceQuestion(ArrayList<String> list) {
